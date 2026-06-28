b5a642425dda451c37d574a93a4520e7f7b1ac87
MAPmattpinheiro <43304649+MAPmattpinheiro@users.noreply.github.com>
2026-06-28 03:14:39 -0400

    Create __init__.py

---
+++ storage/__init__.py
@@ -0,0 +1,12 @@
+from .store import (
+    save_result, load_results, load_latest_results,
+    list_history_dates, delete_result,
+    save_backtest, load_backtest,
+    save_settings, load_settings,
+)
+__all__ = [
+    "save_result", "load_results", "load_latest_results",
+    "list_history_dates", "delete_result",
+    "save_backtest", "load_backtest",
+    "save_settings", "load_settings",
+]
